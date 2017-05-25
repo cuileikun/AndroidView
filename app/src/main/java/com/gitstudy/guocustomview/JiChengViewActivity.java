2fe61bd2d30b5dfe89a61af9d2179f66804fae78
cuileikun <583271702@qq.com>
2017-05-25 17:59:19 +0800

    提交

--- app/src/main/java/com/gitstudy/guocustomview/JiChengViewActivity.java
+++ app/src/main/java/com/gitstudy/guocustomview/JiChengViewActivity.java
@@ -2,7 +2,7 @@ package com.gitstudy.guocustomview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import com.gitstudy.R;
 public class JiChengViewActivity extends AppCompatActivity {
 
     @Override

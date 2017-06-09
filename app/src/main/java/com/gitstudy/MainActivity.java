49eda87bfb6946e7cfe257edfeb0cd4ad9b59fa8
cuileikun <583271702@qq.com>
2017-06-09 15:59:16 +0800

    新手引导界面ShowcastView

--- app/src/main/java/com/gitstudy/MainActivity.java
+++ app/src/main/java/com/gitstudy/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.RelativeLayout;
 import com.gitstudy.convenientbanner.ConvenientBannerActivity;
 import com.gitstudy.fragmenttabhostutils.FragmentTabhostActivity;
 import com.gitstudy.guocustomview.GuoCustomViewActivity;
+import com.gitstudy.showcastview.ShowCastViewActivity;
 import com.qk.applibrary.activity.QkActivity;
 import com.qk.applibrary.listener.TopbarImplListener;
 import com.qk.applibrary.widget.TopbarView;
@@ -17,6 +18,7 @@ public class MainActivity extends QkActivity implements View.OnClickListener {
     private TopbarView top_bar_view;
     private RelativeLayout guo_custom_view_rl;
     private RelativeLayout convenient_banner_rl;
+    private RelativeLayout show_castview_rl;
 
     @Override
     public int getLayoutId() {
@@ -30,6 +32,7 @@ public class MainActivity extends QkActivity implements View.OnClickListener {
         fragment_tabhost_utils_rl = (RelativeLayout) findViewById(R.id.fragment_tabhost_utils_rl);
         guo_custom_view_rl = (RelativeLayout) findViewById(R.id.guo_custom_view_rl);
         convenient_banner_rl = (RelativeLayout) findViewById(R.id.convenient_banner_rl);
+        show_castview_rl = (RelativeLayout) findViewById(R.id.show_castview_rl);
     }
 
     @Override
@@ -45,6 +48,7 @@ public class MainActivity extends QkActivity implements View.OnClickListener {
         fragment_tabhost_utils_rl.setOnClickListener(MainActivity.this);
         guo_custom_view_rl.setOnClickListener(MainActivity.this);
         convenient_banner_rl.setOnClickListener(MainActivity.this);
+        show_castview_rl.setOnClickListener(MainActivity.this);
     }
 
     private TopbarImplListener topListener = new TopbarImplListener() {
@@ -66,7 +70,9 @@ public class MainActivity extends QkActivity implements View.OnClickListener {
             case R.id.convenient_banner_rl:
                 startActivity(new Intent(MainActivity.this,ConvenientBannerActivity.class));
                 break;
-
+            case R.id.show_castview_rl:
+                startActivity(new Intent(MainActivity.this,ShowCastViewActivity.class));
+                break;
 
         }
 
